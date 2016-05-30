c58a9dc93be9d33db217ecb3032f75937111afd0
Hongshen2010clear <yhongshe@stevens.edu>
2016-05-30 15:09:48 -0400

    Add animated cube.

---
+++ WebPage Part/3Dtexturedspin.js
@@ -0,0 +1,76 @@
+var renderer = null;
+    scene = null;
+    camera = null;
+    cube = null;
+    animating = false;
+     
+function onLoad(){
+    var container = document.getElementById("container");
+    //var stats = new Stats();
+    //stats.showPanel(0);
+    //document.body.appendChild(stats.dom);
+    
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
+    container.appendChild(renderer.domElement); 
+    
+    scene = new THREE.Scene();
+    
+    camera = new THREE.PerspectiveCamera(45, container.offsetWidth/container.offsetHeight, 1, 4000);
+    camera.position.set(0, 0, 3);
+    
+    var light = new THREE.DirectionalLight(0xffffff, 1.5);
+    light.position.set(0, 0, 1);
+    scene.add(light);
+    
+    var mapUrl = "Photo/1.jpg";
+    var map = THREE.ImageUtils.loadTexture(mapUrl);
+    
+    var material = new THREE.MeshPhongMaterial({map:map});
+    
+    var geometry = new THREE.CubeGeometry(1, 1, 1);
+    
+    cube = new THREE.Mesh(geometry, material);
+    
+    cube.rotation.x = Math.PI / 5;
+    cube.rotation.y = Math.PI / 5;
+    
+    scene.add(cube);
+    
+    addMouseHandler();
+    
+    run();
+}
+    
+function run(){
+    
+    //stats.begin();
+    
+    renderer.render(scene, camera);
+    
+    if(animating){
+        
+        cube.rotation.y += 0.01;
+        cube.rotation.z += 0.01;
+    
+    }
+    
+    //stats.end();
+    
+    requestAnimationFrame(run);
+}
+    
+function addMouseHandler(){
+    var dom = renderer.domElement;
+    
+    dom.addEventListener('mouseup', onMouseUp, false);
+}
+    
+function onMouseUp(event){
+    event.preventDefault();
+    animating = !animating;
+}
+
+function addCube(){
+    document.getElementById("cube").innerHTML = "Cube added.";    
+}
