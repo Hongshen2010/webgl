c5ea19f21edc394ad66a2fa1f8a8bf3396a52782
Xiang Cao <xcao4@stevens.edu>
2016-06-15 20:07:01 -0400

    preliminary framework of ourwebpage

---
+++ WebPagePart/js/WebGL.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Xiang Cao on 6/15/2016.
+ */
+function addRectangle() {
+    var c=document.getElementById("myCanvas");
+    var cxt=c.getContext("2d");
+    cxt.fillStyle="#FF00FF";
+    cxt.fillRect(300,300,150,75); 
+}
