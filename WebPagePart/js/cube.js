93f8b4383804924b1810041b9e4fc4edf6710b98
Hongshen2010 <yhongshe@stevens.edu>
2016-06-17 14:49:03 -0400

    Misc edit.

--- WebPagePart/js/cube.js
+++ WebPagePart/js/cube.js
@@ -3,7 +3,7 @@ function addCube(){
     var cubeSkinUrl = "Photo/1.jpg";
     var cubeSkin = THREE.ImageUtils.loadTexture(cubeSkinUrl);
     
-    var material = new THREE.MeshPhongMaterial( {map: cubeSkin} );
+    var material = new THREE.MeshPhongMaterial( {color: 0x456789} );
     
     var geometry = new THREE.CubeGeometry(1, 1, 1);
     
